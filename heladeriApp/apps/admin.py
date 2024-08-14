8c6e341f17a20cb18e21fed6539a255ff97f3aea
rodsavic <victorcpcc@gmail.com>
2024-08-14 17:24:30 -0400

    archivos de la carpeta apps

---
+++ heladeriApp/apps/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from apps.clientes.admin import *
+from apps.usuarios.admin import *
+from apps.productos.admin import *
+from apps.proveedores.admin import *
+
+
+# Register your models here.
+
