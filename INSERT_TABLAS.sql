90df83b5e9c93a8bd7988ef6904b92fc3ca4d89e
rodsavic <victorcpcc@gmail.com>
2024-08-14 17:33:39 -0400

    scripts para las tablas

---
+++ INSERT_TABLAS.sql
@@ -0,0 +1,12 @@
+INSERT INTO categoria (descripcion) VALUES ('MATERIA PRIMA');
+INSERT INTO categoria (descripcion) VALUES ('PARA VENTA');
+
+INSERT INTO iva (descripcion) VALUES ('10');
+INSERT INTO iva (descripcion) VALUES ('5');
+
+INSERT INTO medidas(prefijo,descripcion) VALUES('KG', 'Kilogramo');
+INSERT INTO medidas(prefijo,descripcion) VALUES('ML', 'Mililitros');
+INSERT INTO medidas(prefijo,descripcion) VALUES('GR', 'Gramos');
+INSERT INTO medidas(prefijo,descripcion) VALUES('L', 'Litros');
+INSERT INTO medidas(prefijo,descripcion) VALUES('U', 'Unidad');
+
