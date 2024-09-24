f2514d10efb463938782769f8287ced6d948de1c
rodsavic <victorcpcc@gmail.com>
2024-09-24 10:16:14 -0400

    modulo de tipos de pago agregado

--- heladeriApp/apps/admin.py
+++ heladeriApp/apps/admin.py
@@ -5,6 +5,7 @@ from apps.usuarios.admin import *
 from apps.productos.admin import *
 from apps.proveedores.admin import *
 from apps.ventas.admin import *
+from apps.tipo_pago.admin import *
 
 
 # Register your models here.
