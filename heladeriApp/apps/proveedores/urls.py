d95ebb6c07ea0a1ce3dbfdbfdacee42445d0d769
rodsavic <victorcpcc@gmail.com>
2024-10-08 16:50:57 -0300

    CRUD y tabla agregado a proveedor

--- heladeriApp/apps/proveedores/urls.py
+++ heladeriApp/apps/proveedores/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import *
+from apps.proveedores.views import *
 
 app_name = 'proveedores'
 
 urlpatterns = [
     path('', proveedorReadView, name='proveedores'),
-    path('crear_proveedor/', createProveedorView, name='crear_proveedor')
+    path('crear_proveedor/', createProveedorView, name='crear_proveedor'),
+    path('eliminar_proveedor/<str:id_proveedor>',proveedorDeleteView, name='eliminar_proveedor'),
+    path('editar_proveedor/<str:id_proveedor>',proveedorUpdateView, name='editar_proveedor'),
 ]
